0eed6d278a38988416414603f96c5132ce1c4f0a
Theolansink <34719919+Theolansink@users.noreply.github.com>
2021-04-07 15:17:37 +0200

    bash files for installing wordpress with external database

---
+++ database.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade
+sudo apt-get install mysql-server -y
+cd /etc/mysql/mysql.conf.d
+sudo sed -i 's/127.0.0.1/10.0.1.13/g' mysqld.cnf
+sudo mysql -e "CREATE DATABASE wordpress;"
+sudo mysql -e "CREATE USER 'admin'@'10.0.1.12' IDENTIFIED BY 'wordpress';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.*  TO 'admin'@'10.0.1.12';"
+sudo mysql -e "FLUSH PRIVILEGES;"
+cd ~
+sudo service mysql restart
+
---
+++ wordpress.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade
+sudo apt-get install apache2 -y
+sudo apt install php libapache2-mod-php php-mysql -y
+sudo service apache2 start
+sudo systemctl enable apache2
+cd /var/www/html
+sudo wget http://wordpress.org/latest.tar.gz
+sudo tar -xzvf latest.tar.gz
+sudo mv wordpress mysite
+cd mysite
+sudo cp wp-config-sample.php wp-config.php
+sudo sed -i 's/database_name_here/wordpress/g' wp-config.php
+sudo sed -i 's/username_here/admin/g' wp-config.php
+sudo sed -i 's/password_here/wordpress/g' wp-config.php
+sudo sed -i 's/localhost/10.0.1.13/g' wp-config.php
+sudo apt install mysql-client-core-8.0 #install mysql client for testing connection
+sudo service apache2 restart
